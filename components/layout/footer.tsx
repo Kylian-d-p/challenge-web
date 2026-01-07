5610fca05f300f0be317d473df18987b4c304981
Charlemagne <pare.amani05@gmail.com>
2026-01-07 14:07:47 +0100

    refactor: simplify Footer component structure and remove unnecessary React.FC type

--- components/layout/footer.tsx
+++ components/layout/footer.tsx
@@ -1,23 +1,27 @@
-import React from "react";
 import Link from "next/link";
 
-const Footer: React.FC = () => {
+export default function Footer() {
+  return (
+    <footer aria-label="Pied de page">
+      <div>
+        <h3>FIT&FLEX</h3>
+        <p>informations et contacts ci-dessous.</p>
 
-    return (
-        <footer aria-label="Pied de page">
-            <div>
-                <h3>FIT&FLEX</h3>
-                <p>informations et contacts ci-dessous.</p>
-
-                <nav aria-label="Liens du pied de page">
-                    <ul>
-                        <li><Link href="/"><a>Conditions d&apos;utilisation</a></Link></li>
-                        <li><Link href="/privacy"><a>Politique de confidentialité</a></Link></li>
-                    </ul>
-                </nav>
-            </div>
-        </footer>
-    );
-};
-
-export default Footer;
+        <nav aria-label="Liens du pied de page">
+          <ul>
+            <li>
+              <Link href="/">
+                <a>Conditions d&apos;utilisation</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/privacy">
+                <a>Politique de confidentialité</a>
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </footer>
+  );
+}

663366d0849cb5de11a30a16c459caf5a619d819
paolo <paolojoaoparis@gmail.com>
2026-01-07 11:56:42 +0100

    feat: add HeaderNav component with responsive navigation and Sheet UI for mobile feat: implement Sheet component for modal-like navigation overlay

---
+++ components/layout/header-nav.tsx
@@ -0,0 +1,104 @@
+"use client";
+
+import { MenuIcon } from "lucide-react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { useMemo } from "react";
+import { Button } from "../ui/button";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "../ui/sheet";
+
+export default function HeaderNav(props: { user?: { name: string } }) {
+  const pathname = usePathname();
+
+  // /admin/courses - Les cours
+  // /admin/customers - Les utilisateurs
+
+  const navLinks = useMemo(
+    () => [
+      ...(pathname.startsWith("/admin")
+        ? [
+            {
+              title: "Dashboard",
+              href: "/admin/dashboard",
+            },
+            {
+              title: "Utilisateurs",
+              href: "/admin/users",
+            },
+          ]
+        : [
+            {
+              title: "Planning",
+              href: "/planning",
+            },
+            {
+              title: "Reservation",
+              href: "/reservation",
+            },
+          ]),
+      ...(props.user
+        ? [
+            {
+              title: "Mon compte",
+              href: "/account",
+            },
+          ]
+        : [
+            {
+              title: "Connexion",
+              href: "/login",
+            },
+          ]),
+    ],
+    [pathname, props.user]
+  );
+
+  return (
+    <>
+      <Sheet>
+        <SheetTrigger asChild>
+          <Button className="md:hidden" variant={"ghost"} size={"icon"}>
+            <MenuIcon className="size-5" />
+          </Button>
+        </SheetTrigger>
+        <SheetContent>
+          <SheetHeader>
+            <SheetTitle>Menu</SheetTitle>
+            <SheetDescription>Navigation principale</SheetDescription>
+          </SheetHeader>
+          <nav className="flex flex-col items-start gap-4 mt-4">
+            {navLinks.map((link) => (
+              <div key={link.href} className="border-b w-full">
+                <Button
+                  className="text-foreground hover:text-primary"
+                  variant={"link"}
+                >
+                  <Link href={link.href}>{link.title}</Link>
+                </Button>
+              </div>
+            ))}
+          </nav>
+        </SheetContent>
+      </Sheet>
+
+      <nav className="hidden gap-6 md:flex">
+        {navLinks.map((link) => (
+          <Button
+            key={link.href}
+            className="text-foreground hover:text-primary"
+            variant={"link"}
+          >
+            <Link href={link.href}>{link.title}</Link>
+          </Button>
+        ))}
+      </nav>
+    </>
+  );
+}

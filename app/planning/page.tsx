d5dceb4d83658eae7def0cd43fb6a48d8dacc89a
Kylian Dupouy Peron <kdperon@yahoo.com>
2026-01-09 15:39:32 +0100

    refactor: simplify course data handling by removing user booking transformation

--- app/planning/page.tsx
+++ app/planning/page.tsx
@@ -20,29 +20,15 @@ export default async function PlanningPage() {
           userId: true,
         },
       },
-      ...(session && {
-        bookings: {
-          where: {
-            userId: session.user.id,
-          },
-          take: 1,
-        },
-      }),
     },
     orderBy: {
       startDateTime: "asc",
     },
   });
 
-  // Transformer les données pour le client
-  const coursesWithUserBooking = courses.map((course) => ({
-    ...course,
-    userBooking: session && "bookings" in course ? course.bookings[0] || null : null,
-  }));
-
   return (
     <div className="container mx-auto py-8">
-      <PlanningClient courses={coursesWithUserBooking} isAuthenticated={!!session} />
+      <PlanningClient courses={courses} isAuthenticated={!!session} />
     </div>
   );
 }
